406ea3a47a85971af3cf1291cf521547dd0abc8d
AlexandreG83 <alexandre.guillamon83@gmail.com>
2025-12-09 14:22:21 +0000

    if (tree->left)         left_height = 1 + binary_tree_height(tree->left);     if (tree->right)         right_height = 1 + binary_tree_height(tree->right);

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,18 +8,17 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-size_t left_height, right_height;
+	size_t left_height = 0, right_height = 0;
 
-if (tree == NULL)
-	return (0);
+	if (tree == NULL)
+		return (0);
 
-if (tree->left == NULL && tree->right == NULL)
-	return (0);
+	if (tree->left)
+		left_height = 1 + binary_tree_height(tree->left);
+	if (tree->right)
+		right_height = 1 + binary_tree_height(tree->right);
 
-left_height = binary_tree_height(tree->left);
-right_height = binary_tree_height(tree->right);
-
-return (1 + (left_height > right_height ? left_height : right_height));
+	return (left_height > right_height ? left_height : right_height);
 }
 
 /**
@@ -34,5 +33,5 @@ int binary_tree_balance(const binary_tree_t *tree)
 		return (0);
 
 	return (binary_tree_height(tree->left) -
-		binary_tree_height(tree->right));
+binary_tree_height(tree->right));
 }
